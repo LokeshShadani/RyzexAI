23074b9ffcdcb318ce0e9ac99c10aa7d1ffb7b10
Lokesh Shadani <127233702+LokeshShadani@users.noreply.github.com>
2025-07-18 13:35:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def chat():
 
     url = "https://api.groq.com/openai/v1/chat/completions"
     headers = {
-        "Authorization": "Bearer YOUR_API_KEY",
+        "Authorization": "Bearer gsk_KimAcC9HRQ62Vfhah3KiWGdyb3FYcfbI4V13HFdc65y4f7QxVF",
         "Content-Type": "application/json"
     }
 
